7de82f3217126fb7498208ab47dbbeeab9bacf28
wdspider <wdspider@68c66e60-fc3b-0410-8fd4-45e83bb7c2a0>
2007-10-30 00:14:36 +0000

    Added the UI Prototype stuff.  Still need to tie it all together.

--- trunk/DRToolbox.Techniques/PCA.cs
+++ trunk/DRToolbox.Techniques/PCA.cs
@@ -18,7 +18,7 @@ namespace Techniques
         //public PCA(MathNet.Numerics.LinearAlgebra.Matrix A)
         public PCA(Matrix A)
         {
-            aMatrix = A;
+           // aMatrix = A;
 
             //columns = n; // no_dims = number of columns
         }
@@ -30,20 +30,20 @@ namespace Techniques
             //aMatrix = A - repmat(mean(A,1), [size(A,1) 1]); // Abhishikth needs to change to repmat params
             // <summary>Subtraction of matrices</summary>
 		    // public static Matrix operator -(Matrix m1, Matrix m2)
-
+            Matrix aMatrix = null;  // Added by Bobby so code would build
             // Get the covariance
             //Matrix B = cov(aMatrix);
 
             // Perform eigendecomposition
             // blah = eig(something); // Returns TWO matrices!  A matrix of eigen-vectors(In columns) and a diagonal matrix of eigen-values
-
-            Matrix eigenVectors; //= new Matrix();// = something;
-            Matrix eigenValues;
+            Matrix eigenVectors = null; // Added by Bobby so code would build
+            //= new Matrix();// = something;
+            Matrix eigenValues; 
             Matrix lambda;// = new Matrix();// = something;
             //lambda = diag(eigenValues); // J L will do this
             // Sort eigen values
 
-            Matrix sortedLambdas = eigenSort(lambda);
+      //      Matrix sortedLambdas = eigenSort(lambda);
 
             // Sort the matrix of eigenvectors according to the indices in the sortedLambdas matrix(second column is indices)
             // Depends on return values from eigendecomposition and sortedLambdas
--- trunk/DRToolbox.UI/MainForm.cs
+++ trunk/DRToolbox.UI/MainForm.cs
@@ -6,13 +6,102 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using DRToolbox.UI.FileImport.CSV;
+using DRToolbox.UI.Graphs;
+
 namespace DRToolbox.UI
 {
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class MainForm : Form
+    {
+        #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Event Handler
+        /// <summary>
+        /// Click File -> Exit
+        /// </summary>
+        private void mItemExit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exit Application", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void menuImportFileCSV_Click(object sender, EventArgs e)
+        {
+            // Local Variables
+            CSVImportDialog importDialog = new CSVImportDialog();
+
+            // Was import dialog box successful?
+            if (importDialog.ShowDialog(this) == DialogResult.OK)
+            {	// Yes
+                // Show Results
+                MessageBox.Show("File '" + importDialog.ImportFile.FileName + "' was imported.");
+            }
+            else
+            {	// No
+                // Show error
+                MessageBox.Show("Import was canceled.");
+            }
+        }
+
+        /// <summary>
+        /// Click Help -> About
+        /// </summary>
+        private void mItemAbout_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string aboutMessage = "This is a prototype.";
+                MessageBox.Show(aboutMessage, "About DR Toolbox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exit Application", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Click DR Techniques -> Principle Component Analysis (PCA)
+        /// </summary>
+        private void mItemPCA_Click(object sender, EventArgs e)
+        {
+            //TEST
+            //-------------------------------
+            XYScatterPlot xyPlot = new XYScatterPlot();
+            xyPlot.GraphTitle = "Principle Component Analysis (PCA)";
+            xyPlot.XAxisTitle = "X-Axis TEST";
+            xyPlot.YAxisTitle = "Y-Axis TEST";
+
+            ZedGraph.PointPairList list = new ZedGraph.PointPairList();
+            Random rand = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                double x = rand.NextDouble() * 20.0 + 1;
+                double y = Math.Log(10.0 * (x - 1.0) + 1.0) * (rand.NextDouble() * 0.2 + 0.9);
+                list.Add(x, y);
+            }
+            xyPlot.GraphDataList = list;
+
+            xyPlot.MdiParent = this;
+            xyPlot.DrawXYScatterPlot();
+            xyPlot.Show();           
+            //-------------------------------
+        }
+        #endregion
+
+
+
+    }
 }
