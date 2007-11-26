d4da4a524ec91772ee1bd934a84168bb7f8a276f
johnny.darkman <johnny.darkman@68c66e60-fc3b-0410-8fd4-45e83bb7c2a0>
2007-11-26 19:32:44 +0000

    Latest updates

--- trunk/DRToolbox.Utilites/ListConversions.cs
+++ trunk/DRToolbox.Utilites/ListConversions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 
 using MathNet.Numerics.LinearAlgebra;
 using ZedGraph;
@@ -40,6 +41,43 @@ namespace DRToolbox.Utilites
 
             //return the pointpairlist
             return pointList;
+        }
+
+        /// <summary>
+        /// Convert a Matrix to a DataTable
+        /// </summary>
+        public static DataTable MatrixToDataTable(Matrix techniqueResults)
+        {
+            //Local Vars
+            DataTable dtData = new DataTable();
+
+            //Create columns in datatable from the matrix
+            for (int i = 1; i < techniqueResults.ColumnCount + 1; i++)
+            {
+                DataColumn dcData = new DataColumn();
+                dcData.ColumnName = "Dimension " + Convert.ToString(i);
+                dtData.Columns.Add(dcData);
+            }
+
+            //Add each row in the matrix to the datatable
+            for (int i = 0; i < techniqueResults.RowCount; i++)
+            {
+                //New row in the data table
+                DataRow drData = dtData.NewRow();
+
+                for (int j = 1; j < dtData.Columns.Count + 1; j++)
+                {
+                    //Add columns to the row
+                    string columnName = "Dimension " + j.ToString();
+                    drData[columnName] = techniqueResults[i, j-1];
+                }
+
+                //Add row to the table
+                dtData.Rows.Add(drData);
+            }
+
+            //Return the datatable
+            return dtData;
         }
         #endregion
     }
